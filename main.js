d6d7f682672b54d66d019aa2cf992547b4ad1b84
Elena Tschimev <elena@dev-thought.cool>
2018-10-28 23:08:55 +0100

    - add feature to save the new add contact item

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-let contactList = createMockData(30);
+let contactList = createMockData(3);
 
 $(document).ready(() => {
-  $('.contact-details .input-content').addClass('hide');
-  $('button.btn.save').addClass('hide');
-
+  hideEditableMode();
   $('.btn.add').on('click', () => {
     $('.contact-details .input-content').removeClass('hide');
     $('button.btn.save').removeClass('hide');
   });
+
   $('input').attr('required', true);
+
   $('#addContactForm').submit(function(event) {
+    insertNewContactItem($(this).serializeArray());
+    insertContactItems(contactList);
     alert('Handler for .submit() called.');
     $('.input-content input').val('');
-
-    // event.preventDefault();
+    hideEditableMode();
+    event.preventDefault();
   });
 
   insertContactItems(contactList);
@@ -23,7 +25,7 @@ function createMockData(counter) {
   let contactListMock = [];
   for (let i = 0; i <= counter; i++) {
     faker.seed(i + 1);
-    let contactItem = {
+    contactItem = {
       name: faker.name.firstName(),
       surname: faker.name.lastName(),
       phoneNumber: faker.phone.phoneNumber(),
@@ -37,7 +39,22 @@ function createMockData(counter) {
 }
 
 function insertContactItems(contactList) {
+  let myNewItem = '';
   contactList.forEach(listItem => {
-    $('.contact-list').append(` <div class="margin20"> ${listItem.name}  ${listItem.surname}<hr> </div>`);
+    myNewItem += ` <div class="margin20"> ${listItem.name}  ${listItem.surname}<hr> </div>`;
   });
+  $('.contact-list').html(myNewItem);
+}
+
+function insertNewContactItem(newContact) {
+  const myNewContact = {};
+  newContact.forEach(inputItem => {
+    myNewContact[inputItem.name] = inputItem.value;
+  });
+  contactList.push(myNewContact);
+}
+
+function hideEditableMode() {
+  $('.contact-details .input-content').addClass('hide');
+  $('button.btn.save').addClass('hide');
 }
